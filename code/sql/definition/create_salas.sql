dc8601527fa0a09fa167056330a2aa2e45421cce
HasDieD013 <158527945+HasDieD013@users.noreply.github.com>
2024-02-09 14:27:33 +0000

    create corrigido junior

--- code/sql/definition/create_salas.sql
+++ code/sql/definition/create_salas.sql
@@ -1,7 +1,8 @@
-CREATE TABLE salas
+CREATE TABLE salas(
     id_sala VARCHAR PRIMARY KEY,
     nome_sala VARCHAR(20),
     tipo_sala   VARCHAR(20),
     lousa_digital BOOLEAN,
     projetor BOOLEAN,
     ar_condicionado BOOLEAN
+);
