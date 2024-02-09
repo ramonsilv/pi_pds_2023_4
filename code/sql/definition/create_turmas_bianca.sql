72fa9b4bc118de0ea3b769dceedb56990acac414
bioncalima <159536989+bioncalima@users.noreply.github.com>
2024-02-09 20:54:33 -0300

    Update create_turmas_bianca.sql

--- code/sql/definition/create_turmas_bianca.sql
+++ code/sql/definition/create_turmas_bianca.sql
@@ -1,8 +1,10 @@
 CREATE TABLE "turmas" (
-  "id_turma" varchar PRIMARY KEY,
-  "turno" varchar NOT NULL,
-  "data_inicio" date NOT NULL,
-  "data_fim" date,
-  "horas_aula_dia" integer NOT NULL,
-  "fk_curso" varchar REFERENCES cursos(id_curso)
+	"id_turma" varchar PRIMARY KEY,
+	"polo_educacional" varchar NOT NULL,
+	"turno" varchar NOT NULL,
+	"presencial" boolean,
+	"data_inicio" date NOT NULL,
+	"data_fim" date,
+	"horas_aula_dia" integer NOT NULL,
+	"fk_curso" varchar
 );
