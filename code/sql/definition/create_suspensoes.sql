8df292183ecda315e57a3e2ffdb1da172a19d391
ILTON JUNIO CAETANO DE SOUZA <129558260+iltonjunio@users.noreply.github.com>
2024-02-09 14:38:21 +0000

    update

--- code/sql/definition/create_suspensoes.sql
+++ code/sql/definition/create_suspensoes.sql
@@ -3,3 +3,19 @@ id_aulas varchar NOT NULL,
 id_suspensao varchar NOT NULL,
 primary key (id_aulas,id_suspensao)
 );
+
+ALTER TABLE "turmas" ADD FOREIGN KEY ("fk_curso") REFERENCES "cursos" ("id_curso");
+
+ALTER TABLE "unidades" ADD FOREIGN KEY ("fk_curso") REFERENCES "cursos" ("id_curso");
+
+ALTER TABLE "aulas" ADD FOREIGN KEY ("turma") REFERENCES "turmas" ("id_turma");
+
+ALTER TABLE "aulas" ADD FOREIGN KEY ("unidade") REFERENCES "unidades" ("id_unidade");
+
+ALTER TABLE "aulas" ADD FOREIGN KEY ("sala") REFERENCES "salas" ("id_sala");
+
+ALTER TABLE "aulas_suspensoes" ADD FOREIGN KEY ("id_aulas") REFERENCES "aulas" ("id_aula");
+
+ALTER TABLE "aulas_suspensoes" ADD FOREIGN KEY ("id_suspensao") REFERENCES "suspensoes" ("id_suspensao");
+
+ALTER TABLE aulas ALTER COLUMN "suspensao" DROP NOT NULL;
