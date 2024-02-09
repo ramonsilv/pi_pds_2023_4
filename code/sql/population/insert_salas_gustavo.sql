3bcfda4a148f184cc16a88dcf24661a12903214b
xgoldzin <159536334+xgoldzin@users.noreply.github.com>
2024-02-09 13:59:15 +0000

    insert_salas_gustavo

---
+++ code/sql/population/insert_salas_gustavo.sql
@@ -0,0 +1,11 @@
+INSERT INTO salas (id_sala,nome_sala,tipo_sala,lousa_digital,projetor,ar_condicionado) VALUES
+('S-01','Informática','Sala de Aula', true, false, true),
+('S-02','Morfologia','Laboratório', true, true, true),
+('S-03','Estética','Auditório', true, false, true),
+('S-04','Design Gráfico','Sala de Aula', false, false, true),
+('S-05','História','Sala de Aula', false, false, false),
+('S-06','Análise e Desenvolvimento de Sistemas','Auditório', true, false, true),
+('S-07','Jovem apreniz','Sala de Aula', false, true, true),
+('S-08','Química','Laboratório', false, false, false),
+('S-09','Física','Sala de Aula', true, false, true),
+('S-10','Geometria','Auditório', true, true, false);
