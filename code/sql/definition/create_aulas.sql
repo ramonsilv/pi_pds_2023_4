b1a6e73080b189efe60823d9776eee1434114a72
Prof. Danielle Almeida <159446836+profdaniellegms@users.noreply.github.com>
2024-02-14 08:41:42 -0300

    correção create_aulas.sql

--- code/sql/definition/create_aulas.sql
+++ code/sql/definition/create_aulas.sql
@@ -1,24 +1,8 @@
 CREATE TABLE aulas (
 	"id_aula" VARCHAR PRIMARY KEY,
-	"data_aula" DATE DEFAULT 'NOW()',
-	"status_aula" VARCHAR DEFAULT 'pendente',
+	"data_aula" DATE DEFAULT 'CURRENT_DATE',
+	"status_aula" VARCHAR DEFAULT 'Prevista' CHECK "status_aula" IN ('Prevista', 'Ministrada', 'Suspensa'),
 	"turma" VARCHAR NOT NULL REFERENCES "turmas" ("id_turma"),
 	"unidade" VARCHAR NOT NULL REFERENCES "unidades" ("id_unidade"),
 	"sala" VARCHAR NOT NULL REFERENCES "salas" ("id_sala")
 );
-
-ALTER TABLE "turmas" ADD FOREIGN KEY ("fk_curso") REFERENCES "cursos" ("id_curso");
-
-ALTER TABLE "unidades" ADD FOREIGN KEY ("fk_curso") REFERENCES "cursos" ("id_curso");
-
-ALTER TABLE "aulas" ADD FOREIGN KEY ("turma") REFERENCES "turmas" ("id_turma");
-
-ALTER TABLE "aulas" ADD FOREIGN KEY ("unidade") REFERENCES "unidades" ("id_unidade");
-
-ALTER TABLE "aulas" ADD FOREIGN KEY ("sala") REFERENCES "salas" ("id_sala");
-
-ALTER TABLE "aulas_suspensoes" ADD FOREIGN KEY ("id_aulas") REFERENCES "aulas" ("id_aula");
-
-ALTER TABLE "aulas_suspensoes" ADD FOREIGN KEY ("id_suspensao") REFERENCES "suspensoes" ("id_suspensao");
-
-ALTER TABLE aulas ALTER COLUMN "suspensao" DROP NOT NULL;
