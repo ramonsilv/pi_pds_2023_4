a7be7724d5f08f4f325c31b58d31f0941e80f7ad
Prof. Danielle Almeida <159446836+profdaniellegms@users.noreply.github.com>
2024-02-14 08:47:16 -0300

    correção parte 2 create_aulas.sql

--- code/sql/definition/create_aulas.sql
+++ code/sql/definition/create_aulas.sql
@@ -2,7 +2,7 @@ CREATE TABLE aulas (
 	"id_aula" VARCHAR PRIMARY KEY,
 	"data_aula" DATE DEFAULT 'CURRENT_DATE',
 	"status_aula" VARCHAR DEFAULT 'Prevista' CHECK "status_aula" IN ('Prevista', 'Ministrada', 'Suspensa'),
-	"turma" VARCHAR NOT NULL REFERENCES "turmas" ("id_turma"),
-	"unidade" VARCHAR NOT NULL REFERENCES "unidades" ("id_unidade"),
-	"sala" VARCHAR NOT NULL REFERENCES "salas" ("id_sala")
+	"fk_turma" VARCHAR NOT NULL REFERENCES "turmas" ("id_turma"),
+	"fk_unidade" VARCHAR NOT NULL REFERENCES "unidades" ("id_unidade"),
+	"fk_sala" VARCHAR NOT NULL REFERENCES "salas" ("id_sala")
 );
