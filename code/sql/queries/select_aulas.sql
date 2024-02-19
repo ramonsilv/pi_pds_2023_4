c4727d042b38017515696a3087e35358423c7844
ILTON JUNIO CAETANO DE SOUZA <129558260+iltonjunio@users.noreply.github.com>
2024-02-19 14:15:22 +0000

    update

--- code/sql/queries/select_aulas.sql
+++ code/sql/queries/select_aulas.sql
@@ -33,3 +33,42 @@ GROUP BY turma;
 
 -- Selecionar todas as contagens de aulas por turma a partir da view criada
 SELECT * FROM aulas_por_turma;
+
+--Contagem de aulas por status:
+CREATE VIEW contagem_aulas_status AS
+SELECT status_aula AS Status, COUNT(*) AS Quantidade
+FROM aulas
+GROUP BY status_aula
+ORDER BY Quantidade DESC;
+
+select * from contagem_aulas_status
+
+--Contagem de aulas por mês:
+CREATE VIEW contagem_aulas_mes AS
+SELECT EXTRACT(MONTH FROM data_aula) AS Mês, COUNT(*) AS Quantidade
+FROM aulas
+GROUP BY EXTRACT(MONTH FROM data_aula)
+ORDER BY Mês;
+
+select * from contagem_aulas_mes
+--Contagem de aulas por turma:
+
+CREATE VIEW contagem_aulas_turma AS
+SELECT fk_turma AS "ID da Turma", COUNT(*) AS Quantidade
+FROM aulas
+GROUP BY fk_turma;
+
+select * from contagem_aulas_turma
+--Suspensões de uma turma específica:
+
+CREATE VIEW suspensao_por_turma AS
+SELECT a.data_aula AS "Data da Aula", s.motivo_suspensao AS "Motivo da Suspensão", 
+       CASE WHEN s.reposicao THEN 'Sim' ELSE 'Não' END AS "Reposição"
+FROM aulas_suspensoes a_s
+JOIN suspensoes s ON a_s.id_suspensao = s.id_suspensao
+JOIN aulas a ON a_s.id_aulas = a.id_aula
+WHERE a.fk_turma = 't-01' 
+ORDER BY a.data_aula DESC;
+
+select * from suspensao_por_turma
+
